b05dd80b9d84ed6aa80021c8f5e1f9f664eed914
Mustakimur Rahman <mustakcsecuet@gmail.com>
2021-03-25 12:18:34 -0400

    file code

--- files/src/main.rs
+++ files/src/main.rs
@@ -1,10 +1,100 @@
+use std::error::Error;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io;
+use std::io::prelude::*;
+use std::io::ErrorKind;
+use std::io::Read;
+use std::io::{BufRead, BufReader, Write};
+use std::{env, fs};
+
 fn main() {
-    file_append_write();
-    directory_traversal();
+    panic_msg();
+
+    //file_01();
+
+    //file_02();
+
+    //file_03();
+
+    //file_04();
+
+    //file_append_write();
+
+    //file_05();
+
+    // next lecture
+    //directory_traversal();
 }
 
-use std::fs::OpenOptions;
-use std::io::prelude::*;
+fn panic_msg() {
+    let v = vec![1, 2, 3];
+
+    panic!("Let's create a panic!!!");
+    //v[99];
+}
+
+fn file_01() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+}
+
+fn file_02() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
+    };
+}
+
+fn file_03() {
+    let f = File::open("hello2.txt").unwrap();
+    //let f = File::open("hello2.txt").expect("Failed to open hello.txt");
+}
+
+fn file_04() {
+    match read_username_from_file() {
+        Ok(str) => println!("Message Received: {}", str),
+        Err(e) => eprintln!("There is an error of {}", e),
+    }
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+/* fn read_username_from_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+} */
 
 fn file_append_write() {
     let mut file = OpenOptions::new()
@@ -18,8 +108,21 @@ fn file_append_write() {
     }
 }
 
-use std::error::Error;
-use std::{env, fs};
+fn file_05() -> Result<(), io::Error> {
+    let path = "lines.txt";
+
+    let mut output = File::create(path)?;
+    write!(output, "Rust\nis\nFun")?;
+
+    let input = File::open(path)?;
+    let buffered = BufReader::new(input);
+
+    for line in buffered.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
+}
 
 fn directory_traversal() -> Result<(), Box<dyn Error>> {
     let current_dir = env::current_dir()?;
