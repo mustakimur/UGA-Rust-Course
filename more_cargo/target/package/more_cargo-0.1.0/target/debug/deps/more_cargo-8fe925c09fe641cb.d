8297c4bc8cef07dd44b7f16f65b59129442ffd71
mustakimur khandaker <mrkhandaker@uga.edu>
2022-04-03 23:40:25 -0400

    crates.io

--- more_cargo/target/package/more_cargo-0.1.0/target/debug/deps/more_cargo-8fe925c09fe641cb.d
+++ more_cargo/target/package/more_cargo-0.1.0/target/debug/deps/more_cargo-8fe925c09fe641cb.d
@@ -1,7 +1,7 @@
-/home/mustakimur/teaching/secureprogramming-8245/UGA-Rust-Course/more_cargo/target/debug/deps/more_cargo-8fe925c09fe641cb.rmeta: src/lib.rs
+/home/mustakimur/teaching/secureprogramming-8245/UGA-Rust-Course/more_cargo/target/package/more_cargo-0.1.0/target/debug/deps/more_cargo-8fe925c09fe641cb.rmeta: src/lib.rs
 
-/home/mustakimur/teaching/secureprogramming-8245/UGA-Rust-Course/more_cargo/target/debug/deps/libmore_cargo-8fe925c09fe641cb.rlib: src/lib.rs
+/home/mustakimur/teaching/secureprogramming-8245/UGA-Rust-Course/more_cargo/target/package/more_cargo-0.1.0/target/debug/deps/libmore_cargo-8fe925c09fe641cb.rlib: src/lib.rs
 
-/home/mustakimur/teaching/secureprogramming-8245/UGA-Rust-Course/more_cargo/target/debug/deps/more_cargo-8fe925c09fe641cb.d: src/lib.rs
+/home/mustakimur/teaching/secureprogramming-8245/UGA-Rust-Course/more_cargo/target/package/more_cargo-0.1.0/target/debug/deps/more_cargo-8fe925c09fe641cb.d: src/lib.rs
 
 src/lib.rs:
